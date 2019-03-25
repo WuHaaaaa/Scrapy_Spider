638a25aab2f18df800ed67adee9d769944c04875
WuHaaaaa <ruanheng159@163.com>
2019-03-26 07:13:19 +0800

    腾讯招聘-Scrapy

---
+++ tutorial/items.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TutorialItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+class RecruitItem(scrapy.item):
+    """
+    定义腾讯招聘爬取字段
+    """
+    name = scrapy.Field()
+    detailLink = scrapy.Field()
+    catalog = scrapy.Field()
+    recruitNumber = scrapy.Field()
+    workLocation = scrapy.Field()
+    publishTime = scrapy.Field()
+
---
+++ tutorial/spiders/tencent_spider.py
@@ -0,0 +1,31 @@
+import scrapy
+
+class RecruitSpider(scrapy.Spider):
+    """
+    创建一个Spider，继承自scrapy.Spider
+    """
+    # spider名称
+    name = "tencent"
+    # 
+    allowed_domains = ["hr.tencent.com"]
+    # 初始URL列表
+    start_urls = [
+        "https://hr.tencent.com/position.php?&start=0#a",
+    ]
+
+
+    def parse(self,response):
+        """
+        解析返回的网页数据，提取结构化数据，生成下一页需要请求的URL
+        """
+
+        for sel in response.xpath('//*[@class="even"]'):
+            name = sel.xpath("./td[1]/a/text()").extract()[0]
+            detailLink = sel.xpath("./td[1]/a/@href").extract()[0]
+            catalog = sel.xpath("./td[2]/text()").extract()[0]
+            recruitNumber = sel.xpath('./td[3]/text()').extract()[0]
+            workLocation = sel.xpath('./td[4]/text()').extract()[0]
+            publishTime = sel.xpath('./td[5]/text()').extract()[0]
+            with open('tencetn.csv','a+') as f:
+                f.write(name +","+ detailLink +","+ catalog +","+ str(recruitNumber) +","+ workLocation+","+ str(publishTime) + '\n')
+            print(name," ",detailLink," ", catalog," ", recruitNumber, " ", workLocation, " ", publishTime)
